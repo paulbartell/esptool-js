59ddcea7874ee309ca589802045408c493cd05e2
Paul Bartell <pbartell@amazon.com>
2021-11-23 12:45:23 -0800

    Add ftdi vid
    Add ftdi vendor id.

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ connectButton.onclick = async () => {
 
     if (device === null) {
         device = await navigator.serial.requestPort({
-            filters: [{ usbVendorId: 0x10c4 }]
+            filters: [{ usbVendorId: 0x10c4 }, { usbVendorId: 0x0403 }]
         });
         transport = new Transport(device);
     }
